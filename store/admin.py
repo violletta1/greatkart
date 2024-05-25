655ced2f00528ff8f945a35edc279783a42b6990
Violetta <villygeorgieva@yahoo.com>
2024-05-25 15:38:43 +0300

    Initial

---
+++ store/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from store.models import Product
+
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name','price','stock','category','modified_date')
+    prepopulated_fields = {
+        'slug': ('product_name',)
+    }
+
+
+admin.site.register(Product,ProductAdmin)
---
+++ store/urls.py
@@ -0,0 +1,14 @@
+
+
+
+from django.urls import path
+
+from store import views
+
+
+urlpatterns = [
+
+    path('',views.store, name='store'),
+    path('<slug:category_slug>/',views.store, name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/',views.product_details, name='products_details'),
+] 
