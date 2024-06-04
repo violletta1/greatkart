ed5bd16c26b4243c6e15274e84cf4d741f2d2a40
Violetta <villygeorgieva@yahoo.com>
2024-06-04 15:09:08 +0300

    review and rating

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from store.models import Product, Variation
-
+from store.models import Product, ProductGallery, ReviewRating, Variation
+import admin_thumbnails
 # Register your models here.
+
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name','price','stock','category','modified_date')
@@ -17,3 +22,5 @@ class VariationAdmin(admin.ModelAdmin):
 
 admin.site.register(Variation,VariationAdmin)
 admin.site.register(Product,ProductAdmin)
+admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
