6f2851f809c999f8f611a548553fbd59e8aa961c
Violetta <villygeorgieva@yahoo.com>
2024-06-06 18:33:42 +0300

    add secret key

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # from decouple import config
 from dotenv import load_dotenv
 load_dotenv()
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-+y*3f%hu$9ar8(s2#0j&u^x5#lu@4yy4@8jur1(me+osepw*ry'
 
 # Debug
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'  # Convert to boolean
