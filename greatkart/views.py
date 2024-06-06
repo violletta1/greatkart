50fb516f00762b6e103a91a4a01aea29f9b0e6dd
Violetta <villygeorgieva@yahoo.com>
2024-06-06 18:44:21 +0300

    reviews

--- greatkart/views.py
+++ greatkart/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render
 from store.models import Product, ReviewRating
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by('created_date')
+    products = Product.objects.filter(is_available=True).order_by('created_date')
+    reviews = []  # Initialize as empty list
+    
     for product in products:
-        # Get the reviews
-        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+        # Get the reviews and append them to the list
+        product_reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+        reviews.extend(product_reviews)
         
     context = {
         'products': products,
         'reviews': reviews
     }
     
-    return render(request, 'home.html',context)
+    return render(request, 'home.html', context)
