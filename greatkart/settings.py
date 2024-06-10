948e888a64203e55bb0bb4d89d6bd2671e575bae
Violetta <villygeorgieva@yahoo.com>
2024-06-10 09:40:21 +0300

    change to sqlite

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -93,23 +93,23 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "backendcourse",
-        "USER": "mysuperuser",
-        "PASSWORD": "mysuperuser",
-        "HOST": "backend.c56c46yo0xcq.eu-north-1.rds.amazonaws.com",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "backendcourse",
+#         "USER": "mysuperuser",
+#         "PASSWORD": "mysuperuser",
+#         "HOST": "backend.c56c46yo0xcq.eu-north-1.rds.amazonaws.com",
+#         "PORT": "5432",
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -176,12 +176,12 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
-AWS_ACCESS_KEY_ID = 'AKIAQ3EGT3Z24EYQSNN2'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'greatkartbackend'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'eu-north-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_ACCESS_KEY_ID = 'AKIAQ3EGT3Z24EYQSNN2'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'greatkartbackend'
+# AWS_S3_SIGNATURE_NAME = 's3v4',
+# AWS_S3_REGION_NAME = 'eu-north-1'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL =  None
+# AWS_S3_VERITY = True
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
